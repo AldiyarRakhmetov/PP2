31ae19907694bfb8c735a856295ff10bf227bc83
Aldiyar <a_rakhmetov@kbtu.kz>
2025-03-20 10:42:16 +0500

    Added HW8

--- HW7.py
+++ HW7.py
@@ -1,1 +1,41 @@
-from seperate import seperate as sep
+import pygame
+import datetime
+
+#1
+pygame.init()
+screen = pygame.display.set_mode((680, 680))
+running = True
+
+epic_mickey_clock = pygame.image.load('C:\\Users\\Aldiy\\work\\HW7_files\\HW7_mickeyclock.jpg')
+minute_hand = pygame.image.load('C:\\Users\\Aldiy\\work\\HW7_files\\HW7_minutes.png')
+seconds_hand = pygame.image.load('C:\\Users\\Aldiy\\work\\HW7_files\\HW7_seconds.png')
+
+EMC_rect = epic_mickey_clock.get_rect(center=(340, 340))
+MH_rect = minute_hand.get_rect(center=(340, 340))
+SH_rect = seconds_hand.get_rect(center=(340, 340))
+
+start_time = pygame.time.get_ticks()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    now = datetime.datetime.now()
+    minutes = now.minute
+    seconds = now.second
+
+    rotating_MH = pygame.transform.rotate(minute_hand,  -5.9 * minutes)
+    rotating_SH = pygame.transform.rotate(seconds_hand, -5.9 * seconds)
+    MH_rect_r = rotating_MH.get_rect(center=(340, 340))
+    SH_rect_r = rotating_SH.get_rect(center=(340, 340))
+
+    screen.blit(epic_mickey_clock, EMC_rect)
+    screen.blit(rotating_MH, MH_rect_r)
+    screen.blit(rotating_SH, SH_rect_r)
+
+    pygame.display.flip()
+pygame.quit()
+
+#2
+pygame.init()
